88708e8252164dbaf10e0507a3b2eb2bdbe6b69f
demoleas21 <andrew.demoleas@gmail.com>
2016-01-01 21:49:45 -0500

    more urls added

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+"""Admin site"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Question
+from .models import Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,22 @@
 """View Controller"""
 
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 
 def index(request):
+    """Site Display"""
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def detail(request, question_id):
+    """Question view"""
+    return HttpResponse("You're looking at question %s" % question_id)
 
-# Create your views here.
+def results(request, question_id):
+    """Result view"""
+    response = "You're looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    """Vote view"""
+    return HttpResponse("You're voting on question %s" % question_id)
