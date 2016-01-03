e91d247931319e385be528b9abb3a3f7ff58fdcb
demoleas21 <andrew.demoleas@gmail.com>
2016-01-03 16:11:59 -0500

    Test cases added

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,7 +13,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """Published recently"""
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
