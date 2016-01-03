e19e942db372e8342f8f0112d8333c1d72b3975d
demoleas21 <andrew.demoleas@gmail.com>
2016-01-02 23:01:45 -0500

    Voting working

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,10 +3,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /polls/5/ the 'name' value as called by the {% url %} template tag
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
