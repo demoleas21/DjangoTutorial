315b2a25cd5c7f146d84e1a08b9ce4004be07687
demoleas21 <andrew.demoleas@gmail.com>
2016-01-03 19:28:06 -0500

    admin page completed

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,9 +13,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        """Published recently"""
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        """Recently published"""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
